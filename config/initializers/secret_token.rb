cc0d8cdb0b6d9db6af5252855dcd41de7cd0c9d5
Adam Leonard <adamjamesleonard@gmail.com>
2011-04-05 20:23:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprowty::Application.config.secret_token = '7c5e9321a427e890141759d61c6ccae056e12db1db41cf3d3d118b8283f87348d5acb374fb6b684c68181eccb9b22efad68af81de7ecd8e166f50b1da8a9ea16'
