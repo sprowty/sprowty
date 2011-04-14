9b279e912b1792fec86cf705b8f6352644535263
Adam Leonard <adamjamesleonard@gmail.com>
2011-04-13 21:36:09 -0400

    Added ability to upload a photo

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :picture, :styles => { :thumb => '100x100' }
+  has_attached_file :picture, :styles => { :thumb => '100x100', :profile => "195x175>" }
 end
