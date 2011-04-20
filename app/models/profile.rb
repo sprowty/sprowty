24f1742df7a5dc09eaeb505030b12e72202f6676
Adam Leonard <adamjamesleonard@gmail.com>
2011-04-19 22:20:29 -0400

    Fixes for profile picture

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   
-  attr_accessible :user_attributes, :first_name, :last_name, :location, :about, :paypal, :facebook, :twitter
+  attr_accessible :user_attributes, :first_name, :last_name, :location, :about, :paypal, :facebook, :twitter, :picture
   
-  has_attached_file :picture, :styles => { :thumb => '124x124', :profile => "195x175>" }
+  has_attached_file :picture, :styles => { :thumb => '124x124', :profile => "195x175#" }
   
   accepts_nested_attributes_for :user
 end
