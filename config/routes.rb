978b39d143a57e1ea45da2220b807cb4fd8175a1
Ryan Richards <abstractryan@gmail.com>
2011-09-13 17:47:18 -0500

    activeadmin basic configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Sprowty::Application.routes.draw do
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :messages, :projects, :skills, :bids, :works
 
   resource :profile, :controller => "profiles"
