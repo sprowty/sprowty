7e11e93af6d05664d36bdfeed5eb443f3f083d34
Daniel Brown <daniel.brown@alorus.com>
2011-09-16 11:08:31 -0400

    Fixed colorbox for project create

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @search   = Project.search(params[:search])
-    @projects = Project.paginate(:page => params[:page], :per_page => 6)
+    @projects = Project.paginate(:page => params[:page], :per_page => 6, :order => 'created_at DESC')
   end
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,28 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+	
+	$(".live-link").live('click', function () {
+		$.ajax({type: $(this).attr("method"), url: $(this).attr("href"), dataType: "script"});
+		return false;
+	});
+	
+	$(".live-form").live('submit', function () {
+		$.ajax({type: $(this).attr("method"), url: $(this).attr("action"), data: $(this).serialize(), dataType: "script"});
+		return false;
+	});
+});
+
+// Always send the authenticity_token with ajax
+//$(document).ajaxSend(function(event, request, settings) {
+//    if ( settings.type != 'GET' ) {
+//        settings.data = (settings.data ? settings.data + "&" : "")
+//            + "authenticity_token=" + encodeURIComponent( AUTH_TOKEN );
+//    }
+//});
+
+
 $(document).ajaxSend(function(e, xhr, options) {
   var token = $("meta[name='csrf-token']").attr("content");
   xhr.setRequestHeader("X-CSRF-Token", token);
