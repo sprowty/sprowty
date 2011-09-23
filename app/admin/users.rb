158d037073b1ca355e1b06464e63735ce81261b0
Ryan Richards <abstractryan@gmail.com>
2011-09-23 07:03:17 -0500

    git ignore public/system, add image path link to profiel images in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,15 +12,18 @@ ActiveAdmin.register User do
   end
 
   show :title => :username do
+    p do
+      "Click on thumbnail to toggle approval status"
+    end
     panel "Profile" do
       table do
         tr do
           th "Profile Image"
-          th "Image Approved"
+          th "Image"
         end
         tr do
           td link_to(image_tag(user.profile.picture.url, :size => '100x100'), approve_profile_path(user.profile))
-          td user.profile.image_approved
+          td link_to(user.profile.image_approved, approve_profile_path(user.profile))
         end
       end
     end
@@ -28,7 +31,7 @@ ActiveAdmin.register User do
     panel "Works" do
       table_for(user.works) do
         column(:work) {|work| link_to(image_tag(work.work.url, :size => '100x100'), approve_work_path(work))}
-        column(:image_approved)
+        column(:image_approved) {|work| link_to(work.image_approved, approve_work_path(work))}
       end
     end
     active_admin_comments
