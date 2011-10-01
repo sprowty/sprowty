728222fe2e2c272ae171c758419ed4f6e004cb6b
Ryan Richards <abstractryan@gmail.com>
2011-10-01 11:53:52 -0500

    updates for paypal - diverting to getting adams changes in next

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,72 +1,48 @@
 class PaymentsController < ApplicationController
-  before_filter :fetch_decrypted, :only => 'create'
   respond_to :html, :json
 
   include ActiveMerchant::Billing::Integrations
-  require 'crypto'
   require 'money'
 
   def new
     @payment = Payment.new
-    render :layout => false
   end
 
   def create
-    @payment = current_user.payments.new(params[:payment])
-
-    if @payment.save
-      render :json => { :success => true }
-    else
-      @payment = Payment.find(params[:payment_id])
-      render :json => { :success => 'false', :html => render_to_string(:file => 'new.html.erb', :template => false) }
-    end
+    #This is the method where the HTML form is created and is posted to paypal
   end
 
-  def place_order
-    #@project = Project.find(params[:project_id])
-
-   # if @logged_user#.credits > 0
-   #   render(:action => "confirm_order")
-   #   return
-   # else
-      #place order will have our paypal buttons
-      render(:action => "place_order")
-   #   return
-   # end
+  def notify
+    p request.inspect
+    ipn
   end
 
-private
-  def fetch_decrypted(project = nil)
-
-    # cert_id is the certificate if we see in paypal when we upload our own certificates
-    # cmd _xclick need for buttons
-    # item name is what the user will see at the paypal page
-    # custom and invoice are passthrough vars which we will get back with the asunchronous
-    # notification
-    # no_note and no_shipping means the client want see these extra fields on the paypal payment
-    # page
-    # return is the url the user will be redirected to by paypal when the transaction is completed.
-    decrypted = {
-      "cert_id" => "9LHCT27AYFL78",
-      "cmd" => "_xclick",
-      "business" => "sprowt_1317256762_biz@sprowty.com",
-      "item_name" => "1 Togun",
-      "item_number" => "1",
-      "custom" =>"something to pass to IPN",
-      "amount" => "5",
-      "currency_code" => "USD",
-      "country" => "US",
-      "no_note" => "1",
-      "no_shipping" => "1"
-    }
-
-    if project
-      decrypted.merge!("invoice" => "Another passthrough var", "return" => "http://www.sprowty.com/payments/done?project_id=#{project.id}")
+  def ipn
+    #This is the method that takes care of checking the request object sending the request object again to paypal to check whether
+    #the request is confirmed or genuine, then it logs the corresponding message to a file.
+    notify = Paypal::Notification.new(request.raw_post)
+    if notify.acknowledge
+      begin
+        my_file = File.new("filename.txt","w")
+        if notify.complete?
+          my_file.write "Transaction complete.. add your business logic here"
+          p "Transaction complete.. add your business logic here"
+        else
+          my_file.write "Transaction not complete, ERROR"
+          p "Transaction not complete, ERROR"
+        end
+      rescue => e
+        my_file.write "Amit we have a bug"
+        p "Amit we have a bug"
+      ensure
+        my_file.write "Make sure we logged everything we must"
+        p "Make sure we logged everything we must"
+      end
+        my_file.close
     else
-      decrypted.merge!("return" => "http://www.sprowty.com/payments/done")
+      my_file.write "Another reason to be suspicious"
+      p "Another reason to be suspicious"
     end
-
-    @encrypted_basic = Crypto42::Button.from_hash(decrypted).get_encrypted_text
-    @action_url = PAYPAL_ACCOUNT
+    render :nothing => true
   end
 end
