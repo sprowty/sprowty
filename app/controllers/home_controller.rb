dcb913528bc856cb85de9fcdf3bba246f37d7a52
adamalfi <adamalfi@gmail.com>
2011-10-11 15:54:27 -0400

    home page fixings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @search   = Project.search(params[:search])
-    @projects = Project.paginate(:page => params[:page], :per_page => 6, :order => 'created_at DESC')
+    @projects = Project.paginate(:page => params[:page], :per_page => 5, :order => 'created_at DESC')
     @tags = @projects.map(&:tags).uniq
   end
 end
