c704df0ca8a089b360bfa49e92d720f800db39e0
Ryan Richards <abstractryan@gmail.com>
2012-01-18 18:15:08 -0600

    profile image size, city/state, first/last name on profile

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,8 @@
 module ProfilesHelper
+  def get_location_for(user)
+    unless user.profile.zipcode.blank?
+      zipcode = ZipCode.where(:zipcode => user.profile.zipcode).first
+      "#{zipcode.city.humanize}, #{zipcode.state.humanize}"
+    end
+  end
 end
