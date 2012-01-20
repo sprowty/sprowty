063c0db5c3a970b47b6ef17188a26d146690b859
Ryan Richards <abstractryan@gmail.com>
2012-01-20 10:50:49 -0600

    better db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-# site users
-seeder = User.create({:email => "seeder_sprowty@mailinator.com",
-             :password => "password",
-             :password_confirmation => "password"})
-
-seeder.confirm!
-
-sprowter = User.create({:email => "sprowter_sprowty@mailinator.com",
-             :password => "password",
-             :password_confirmation => "password"})
-
-sprowter.confirm!
-
-[1..10].each do
-  project = Project.create({:user_id => seeder.id,
-                  :title => "Test Project",
-                  :description => "Example project",
-                  :city => "Brooklyn",
-                  :state => "NY",
-                  :tags => "project",
-                  :price => 3245.25,
-                  :due_date => DateTime.now()+2.months})
-
-  project.post
-end
-
 Category.delete_all
 
 CATEGORIES = ["Accounting/Finance", "Customer Services", "Design,Fashion", "Location Based", "Marketing/PR", "Medical/Health", "Music/Audio", "Organizing/Planning", "Other", "Photography", "Programming/Development", "Research", "Social Media", "Translation", "Video", "Writing"]
@@ -39,3 +13,42 @@ CATEGORIES = ["Accounting/Finance", "Customer Services", "Design,Fashion", "Loca
 CATEGORIES.each do |c|
   Category.create({:name=> c})
 end
+
+["seeder","sprowter"].each do |user_str|
+
+  if User.where(:email => "#{user_str}_sprowty@mailinator.com").length > 0
+    User.where(:email => "#{user_str}_sprowty@mailinator.com").first.destroy
+  end
+
+  user = User.create({:email => "#{user_str}_sprowty@mailinator.com",
+                      :password => "password",
+                      :password_confirmation => "password",
+                      :is_admin => false})
+
+  profile = user.build_profile({:first_name => user_str.humanize,
+                                :last_name => 'User',
+                                :zipcode => '73069',
+                                :picture => File.open(File.join(Rails.root,"db","seed_files","#{user_str}_profile.jpg"))})
+  profile.save!
+  user.confirm!
+end
+
+
+posted = Project.create({:user_id => User.where(:email => 'seeder_sprowty@mailinator.com').first.id,
+                         :title => "Posted Project - #{DateTime.now.strftime('%B %d, %Y at %I:%M%p')}",
+                         :description => "Example project thats automatically posted.",
+                         :city => "Brooklyn",
+                         :state => "NY",
+                         :category => "Other",
+                         :price => 3245.25,
+                         :due_date => DateTime.now()+2.months})
+posted.post
+
+unposted = Project.create({:user_id => User.where(:email => 'seeder_sprowty@mailinator.com').first.id,
+                         :title => "Build a standup desk",
+                         :description => "Example project that is initially pending admin approval before posted.",
+                         :city => "Brooklyn",
+                         :state => "NY",
+                         :tags => "project",
+                         :price => 3245.25,
+                         :due_date => DateTime.now()+2.months})
