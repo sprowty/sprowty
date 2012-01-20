5604f2cb80961306c21e12368e24fd3b63dab4c7
Ryan Richards <abstractryan@gmail.com>
2012-01-20 11:03:42 -0600

    dont need after_create for profile since its now nested_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :terms, :profile_attributes
-  after_create :create_profile
+
   after_create :setup_account
 
   validates_presence_of :email
