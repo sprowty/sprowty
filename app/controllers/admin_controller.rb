b570ea3abdd0368b554c6cc7aa02c18cb2f25e36
Ryan Richards <abstractryan@gmail.com>
2012-03-11 13:10:49 -0500

    admin interface for approving projects

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,6 @@ class AdminController < ApplicationController
   respond_to :html, :js
 
   def index
-    @projects = Project.where("sm_state != 'pending_post'").paginate(:page => params[:page], :per_page => 10)
-
+    @projects = Project.where("sm_state != 'posted'").paginate(:page => params[:page], :per_page => 10)
   end
-
 end
