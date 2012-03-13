000a6d7953305c63e679d9e8e08318869b1e070a
Ryan Richards <abstractryan@gmail.com>
2012-03-12 21:52:54 -0500

    home index fixed for category, city, state search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
 
   def index
+
+    @search   = Project.where(:sm_state => 'posted').search(params[:search])
+
     if params[:city] && params[:state]
-      @projects = Project.where(:city => params[:city], :state => params[:state])
+      @search = Project.where(:sm_state => 'posted', :city => params[:city], :state => params[:state]).search(params[:search])
     end
 
     if params[:category] && !params[:category].blank?
-       @projects = Project.where(:category => params[:category])
+      @search = Project.where(:sm_state => 'posted', :category => params[:category]).search(params[:search])
     end
 
-    @search   = Project.where(:sm_state => 'posted').search(params[:search])
     @projects = @search.paginate(:page => params[:page], :per_page => 5, :order => 'created_at desc')
   end
 end
