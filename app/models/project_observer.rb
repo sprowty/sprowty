40d94d6bcec455ca38a0d27dbd8b572e83965552
Ryan Richards <abstractryan@gmail.com>
2012-03-31 23:47:52 -0500

    do not automatically create a projectalert when bid is received at the system level. When a bid is saved it generates a message to the seeder anyway.

--- app/models/project_observer.rb
+++ app/models/project_observer.rb
@@ -2,12 +2,16 @@ class ProjectObserver < ActiveRecord::Observer
   observe :project
 
   def after_transition(project, transition)
-    project.project_alerts.create(:message => transition.to,:occurred => DateTime.now, :alert_type => 'system')
-    case transition.to
-    when "posted"
-      project.waiting_for_bids
-    when "assigned"
-      project.work_started
+    # dont create an alert on bid_received - it causes problems in the layout and when a bid is really saved its sends a different
+    # type of alert anyway
+    unless transition.to == "bid_received"
+      project.project_alerts.create(:message => transition.to,:occurred => DateTime.now, :alert_type => 'system')
+      case transition.to
+      when "posted"
+        project.waiting_for_bids
+      when "assigned"
+        project.work_started
+      end
     end
   end
 end
